83643fca905ba4c9d8c18d56647769878746bf98
André Díaz <andre290800@gmail.com>
2022-04-26 01:50:28 -0500

    FizzbuzzService

---
+++ test/services/FizzbuzzService.test.js
@@ -0,0 +1,35 @@
+const FizzbuzzService = require("../../lib/services/FizzbuzzService");
+
+describe("Unit Tests for FizzbuzzService", () => {
+    test("Assign Fizzbuzz if score is divisible by 3 and 5", () => {
+        const explorer = { score: 15 };
+
+        FizzbuzzService.applyValidationInExplorer(explorer);
+
+        expect(explorer.trick).toBe("FIZZBUZZ");
+    });
+
+    test("Assign Fizz if score is divisible by 3", () => {
+        const explorer = { score: 3 };
+
+        FizzbuzzService.applyValidationInExplorer(explorer);
+
+        expect(explorer.trick).toBe("FIZZ");
+    });
+
+    test("Assign Buzz if score is divisible by 5", () => {
+        const explorer = { score: 5 };
+
+        FizzbuzzService.applyValidationInExplorer(explorer);
+
+        expect(explorer.trick).toBe("BUZZ");
+    });
+
+    test("Assign score if score is not divisible by 3 or 5", () => {
+        const explorer = { score: 7 };
+
+        FizzbuzzService.applyValidationInExplorer(explorer);
+
+        expect(explorer.trick).toBe(7);
+    });
+});
