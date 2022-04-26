732a3629026e4956f6c72c30b859b727450172df
André Díaz <andre290800@gmail.com>
2022-04-26 18:24:40 -0500

    get explorers by mission

--- test/server.test.js
+++ test/server.test.js
@@ -9,4 +9,14 @@ describe("Unit Tests for server", () => {
         expect(res.status).toBe(200);
         expect(res.body.message).toMatch(/welcome/i);
     });
+
+    test("GET /v1/explorers/:mission", async () => {
+        const mission = "node";
+        const res = await http.get(`/v1/explorers/${mission}`);
+
+        expect(res.status).toBe(200);
+        expect(
+            res.body.every((explorer) => explorer.mission === mission)
+        ).toBeTruthy();
+    });
 });
